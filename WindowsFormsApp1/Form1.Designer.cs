02179e8c0daa7ee1fff40207928a498c9df343f5
Jon Li <standuptovictory@outlook.com>
2018-05-17 19:21:40 -0700

    V2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(37, 59);
+            this.button1.Location = new System.Drawing.Point(384, 109);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(204, 258);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
