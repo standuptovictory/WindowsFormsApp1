97c8488836e2a087db36cac7ba7ac8d9fc843838
Jon Li <standuptovictory@outlook.com>
2018-05-17 19:22:58 -0700

    V4

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(384, 109);
+            this.button1.Location = new System.Drawing.Point(43, 102);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(204, 258);
             this.button1.TabIndex = 0;
